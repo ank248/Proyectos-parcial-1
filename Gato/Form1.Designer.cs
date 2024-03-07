82b79c5a98b971c3cffd99615142619b6dc42e3a
ank248 <112523511+ank248@users.noreply.github.com>
2024-03-07 00:51:36 -0600

    Add files via upload

---
+++ Gato/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Gato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Ganador = new System.Windows.Forms.Label();
+            this.FichasGato = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.picGanador = new System.Windows.Forms.PictureBox();
+            this.picReiniciar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGanador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picReiniciar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.Location = new System.Drawing.Point(382, 90);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(25, 28);
+            this.lblPlayer1.TabIndex = 0;
+            this.lblPlayer1.Text = "0";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.Location = new System.Drawing.Point(511, 90);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(25, 28);
+            this.lblPlayer2.TabIndex = 1;
+            this.lblPlayer2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(438, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "VS";
+            // 
+            // Ganador
+            // 
+            this.Ganador.AutoSize = true;
+            this.Ganador.BackColor = System.Drawing.Color.Transparent;
+            this.Ganador.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ganador.Location = new System.Drawing.Point(626, 190);
+            this.Ganador.Name = "Ganador";
+            this.Ganador.Size = new System.Drawing.Size(103, 28);
+            this.Ganador.TabIndex = 3;
+            this.Ganador.Text = "Ganador";
+            // 
+            // FichasGato
+            // 
+            this.FichasGato.BackColor = System.Drawing.Color.Transparent;
+            this.FichasGato.ColumnCount = 3;
+            this.FichasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.54369F));
+            this.FichasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.45631F));
+            this.FichasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 101F));
+            this.FichasGato.Location = new System.Drawing.Point(12, 12);
+            this.FichasGato.Name = "FichasGato";
+            this.FichasGato.RowCount = 3;
+            this.FichasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.26829F));
+            this.FichasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.73171F));
+            this.FichasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 106F));
+            this.FichasGato.Size = new System.Drawing.Size(315, 315);
+            this.FichasGato.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Gato.Properties.Resources.f_1;
+            this.pictureBox1.Location = new System.Drawing.Point(357, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Gato.Properties.Resources.f_2;
+            this.pictureBox2.Location = new System.Drawing.Point(484, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // picGanador
+            // 
+            this.picGanador.BackColor = System.Drawing.Color.Transparent;
+            this.picGanador.Image = global::Gato.Properties.Resources.f_0;
+            this.picGanador.Location = new System.Drawing.Point(587, 12);
+            this.picGanador.Name = "picGanador";
+            this.picGanador.Size = new System.Drawing.Size(175, 175);
+            this.picGanador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picGanador.TabIndex = 7;
+            this.picGanador.TabStop = false;
+            // 
+            // picReiniciar
+            // 
+            this.picReiniciar.BackColor = System.Drawing.Color.Transparent;
+            this.picReiniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picReiniciar.Image = global::Gato.Properties.Resources.Reiniciar;
+            this.picReiniciar.Location = new System.Drawing.Point(97, 349);
+            this.picReiniciar.Name = "picReiniciar";
+            this.picReiniciar.Size = new System.Drawing.Size(150, 50);
+            this.picReiniciar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picReiniciar.TabIndex = 8;
+            this.picReiniciar.TabStop = false;
+            this.picReiniciar.Click += new System.EventHandler(this.picReiniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Gato.Properties.Resources.Fondo;
+            this.ClientSize = new System.Drawing.Size(774, 411);
+            this.Controls.Add(this.picReiniciar);
+            this.Controls.Add(this.picGanador);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.FichasGato);
+            this.Controls.Add(this.Ganador);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGanador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picReiniciar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Ganador;
+        private System.Windows.Forms.TableLayoutPanel FichasGato;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picGanador;
+        private System.Windows.Forms.PictureBox picReiniciar;
+    }
+}
+
